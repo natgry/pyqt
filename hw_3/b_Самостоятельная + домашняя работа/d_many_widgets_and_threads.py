dd66118a7bac165af0c1881388e88e0e2946b4c7
Natalia Gryaznova <n.gryaznova@yadro.com>
2023-05-11 20:58:39 +0300

    Modify d_many_widgets_and_threads.py

--- hw_3/b_Самостоятельная + домашняя работа/d_many_widgets_and_threads.py
+++ hw_3/b_Самостоятельная + домашняя работа/d_many_widgets_and_threads.py
@@ -1,3 +1,31 @@
 """
 Реализовать окно, которое будет объединять в себе сразу два предыдущих виджета
 """
+from PySide6 import QtWidgets, QtCore
+
+from b_systeminfo_widget import SystemInfoWindow
+
+
+class UnitedWindow(QtWidgets.QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.systemWindow = SystemInfoWindow()
+
+        self.initUI()
+
+        #self.weatherWindow =
+
+    def initUI(self):
+        layout = QtWidgets.QHBoxLayout()
+        layout.addWidget(self.systemWindow)
+        self.setLayout(layout)
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication()
+
+    win = UnitedWindow()
+    win.show()
+
+    app.exec()
